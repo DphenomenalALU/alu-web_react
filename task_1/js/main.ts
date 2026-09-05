8d8fff41dd0b4200e6d646c07ef2e7736c8dbc75
Dphenomenal101 <i.salami@alustudent.com>
2026-09-05 22:57:14 +0200

    feat(task_1): add teacher and student abstractions

---
+++ task_1/js/main.ts
@@ -0,0 +1,49 @@
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string,
+): string => `${firstName.charAt(0)}. ${lastName}`;
+
+export interface StudentClassInterface {
+  displayName(): string;
+  workOnHomework(): string;
+}
+
+export interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+export class StudentClass implements StudentClassInterface {
+  private readonly firstName: string;
+
+  private readonly lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
