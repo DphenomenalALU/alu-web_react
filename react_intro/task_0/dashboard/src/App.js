ec69c339306e8c8a22fd8e5107ae3e910317317b
Dphenomenal101 <i.salami@alustudent.com>
2026-09-13 20:29:09 +0200

    feat(react-intro): add basic dashboard

---
+++ react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import logo from './react-intro-project-holberton-logo.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="Holberton logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <main className="App-body">
+        <p>Login to access the full dashboard</p>
+      </main>
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
