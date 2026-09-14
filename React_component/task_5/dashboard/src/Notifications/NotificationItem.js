910149a340573cb1ff4f1441f3c84bf15f229f68
Dphenomenal101 <i.salami@alustudent.com>
2026-09-14 10:18:54 +0200

    feat(react-component): memoize NotificationItem

---
+++ React_component/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function NotificationItem({ id, type, html, value, markAsRead }) {
+  return (
+    <li
+      data-notification-type={type}
+      onClick={() => markAsRead(id)}
+      {...(html ? { dangerouslySetInnerHTML: html } : {})}
+    >
+      {html ? null : value}
+    </li>
+  );
+}
+
+export default React.memo(NotificationItem);
+
+NotificationItem.propTypes = {
+  id: PropTypes.number,
+  html: PropTypes.shape({
+    __html: PropTypes.string.isRequired,
+  }),
+  markAsRead: PropTypes.func,
+  type: PropTypes.string,
+  value: PropTypes.string,
+};
+
+NotificationItem.defaultProps = {
+  id: 0,
+  html: null,
+  markAsRead: () => {},
+  type: 'default',
+  value: '',
+};
