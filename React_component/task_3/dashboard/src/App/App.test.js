13f03e887077eb2c7b9653944744b1224e6b6706
Dphenomenal101 <i.salami@alustudent.com>
2026-09-14 10:16:22 +0200

    test(react-component): cover body sections

--- React_component/task_3/dashboard/src/App/App.test.js
+++ React_component/task_3/dashboard/src/App/App.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import App from './App';
 import Login from '../Login/Login';
 import CourseList from '../CourseList/CourseList';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
 
 test('App renders without crashing', () => {
   shallow(<App />);
@@ -40,7 +41,11 @@ describe('when the user is logged in', () => {
     expect(wrapper.find(Login)).toHaveLength(0);
   });
 
-  test('displays CourseList', () => {
-    expect(wrapper.find(CourseList)).toHaveLength(1);
+  test('displays CourseList inside the Course list section', () => {
+    const section = wrapper.find(BodySectionWithMarginBottom);
+
+    expect(section).toHaveLength(1);
+    expect(section.prop('title')).toBe('Course list');
+    expect(section.prop('children').type).toBe(CourseList);
   });
 });
